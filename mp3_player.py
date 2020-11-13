0831a9d9bcf27e1dc8741dd9c1ad9407a5818b94
BeRussian <nikishuh@gmail.com>
2020-11-13 13:07:42 +0200

    homeWork

--- mp3_player.py
+++ mp3_player.py
@@ -1,24 +1,19 @@
-#import requests
-#import json
-#from lyrics_api import*  # HW: use it
-#79ecf66258437f295418ce9b4eeaaa38 
+import requests
+import json
+ # HW: use it
+from lyrics_api import *
+from song import *
+from playsound import playsound
+
+import pygame
+import time
+
 import math
-from song import Song
-
-
-def read_lyrics(song_name):
-    file_name = f"lyrics\{song_name}.txt"
-    try:
-        with open(file_name, "r") as lyrics_file:
-            lyrics = lyrics_file.read()
-    except FileNotFoundError:
-        lyrics = ""
-    return lyrics
-
+from song import *
 
 class MP3:
     def __init__(self):
-        self.songs = [Song(...), ...]
+        self.songs=[]
         self.create_existing_songs()
 
     def run(self):
@@ -31,8 +26,8 @@ class MP3:
 
             self.play_song()
 
-            #שואל את המשתמש מה ברצונו לעשות(לנגן שיר,לקבל מידע על שיר)
-            self.info()
+            self.print_info_about_song()
+
 
             choice = input("Would you like to exit? (yes/no)\n")
             if choice == "yes":
@@ -44,13 +39,7 @@ class MP3:
             if song.artist not in artists:
                 artists.append(song.artist)
         return artists
-        #########################
-        artists = set()
-        for song in self.songs:
-            artists.add(song.artist)
-        return list(artists)
 
-    #המשתמש מכניס שיר חדש אל תוך מערך השירים
     def create_new_song(self):
         print("would you like to create a new song?")
         choice=input()
@@ -59,61 +48,99 @@ class MP3:
             artist_name=  input("input name of artist: ")
             album_name =input("Enter name of album: ")
             length=input("Enter length of song in seconds: ")
-            self.songsArray.append(Song(song_name,artist_name,album_name,length))   
+            self.songs.append(Song(song_name,artist_name,album_name,length))   
 
     def create_existing_songs(self):
-        #יוצר 4 אובייקטים מסוג שיר
+        #create's 4 songs
+        
         S1=Song("alone","alan walker","all falls down",163)#1
         S2=Song("see you again","noroz","noroz is on fire",250)#2
         S3=Song("all falls down","alan walker","all falls down",163)#3
         S4=Song("Diamond heart","alan walker","all falls down",163)#4
-        self.songsArray.append(S1)
-        self.songsArray.append(S2)
-        self.songsArray.append(S3)
-        self.songsArray.append(S4)
+        self.songs.append(S1)
+        self.songs.append(S2)
+        self.songs.append(S3)
+        self.songs.append(S4)
 
-    def songList(self):
-        #מדפיס את רשימת השירים
-        print("Do you want to see the song lisg?")
+    def print_songs(self):
+        print("Do you want to see the song list?")
         choice=input()
         if choice=="yes":
-            for x in self.songsArray:
-                print(x.get_name_of_song())
+            for x in self.songs:
+                print(x.name)
 
     def play_song(self):
         # HW: try to actually play the song (hint: you need an external package (import and stuff))
         # HW: print lyrics line every second (hint: use time.sleep function)
-        print("Would you like to play a song?")
-        choice=input()
+        path="C:\\Users\\user\\Desktop\\cyberWithRon\\pyhton\mp3-project\\songs\\"
+        artist_name=""
+        choice=input("Would you like to play a song?\n")
         if choice=="yes":
             songName=input("Which song would you like to play?")
-            print (songName)
-            print("playing...")
+            for i in self.songs:
+                if i.name==songName:
+                    artist_name=i.artist
+            if(songName=="alone"):
+                print(self.search_lyrics(artist_name,songName))
+                #playsound(f""+path+songName+".mp3")
+                pygame.init()
+                pygame.mixer.music.load(f""+path+songName+".mp3")
+                pygame.mixer.music.play(0)
+                time.sleep(5)
+                print("5 seconds")
+                 
 
-    def info(self):
-        print("would you like to get information about a spesific song?")
-        choice=input()
+    def caculate_time_between_lines(self,name):
+        for i in self.songs:
+            if i.name==name:
+                length=i.length
+
+    def print_info_about_song(self):
+        choice=input("would you like to get information about a spesific song?\n")
         if choice=="yes":
-            search_type=input("Search song by 1:name,2:artist,3:album (enter digit)")
-            if search_type==1:
-                print("Enter songs name")
-                song_name=input()
+            search_type=input("Search song by 1:name,2:artist,3:album (enter digit)\n")
+            if search_type=="1":
+                song_name=input("Enter songs name\n")
+                for i in self.songs:
+                    if i.name==song_name:
+                        i.toString
+                        
+            if search_type=="2":
+                artist_name=input("Enter artist name\n")
+                for i in self.songs:
+                    if i.artist==artist_name:
+                        i.toString
 
-            if search_type==2:
-                print("Enter artist name")
-                artist_name=input()
+            if search_type=="3":
+                album_name=input("Enter album name\n")
+                for i in self.songs:
+                    if i.album==album_name:
+                        i.toString
 
-            if search_type==3:
-                print("Enter album name")
-                album_name=input()
+    
+   
+
+    
+
+    def search_lyrics(self,artist_name,track_name,):
+        api_call = base_url + lyrics_matcher + format_url + artist_search_parameter + artist_name + track_search_parameter + track_name + api_key
+        request = requests.get(api_call)
+        data = request.json()
+        data = data['message']['body']
+        print("API Call: " + api_call)
+        print()
+        lyrics=(data['lyrics']['lyrics_body'])
+        return lyrics
+
+        
 
 # HW: now that you know python much better, fix all the code to be better
 
 if __name__ == "__main__":
-    # mp3 = MP3()
-    # mp3.run()
-    lyrics = read_lyrics("baby")
-    if lyrics is not "":
-        print(lyrics)
-    else:
-        print("this song doesn't have any lyrics... :(")
+    #create's new mp3 player
+    mp3 = MP3()
+    #runs the mp3 player
+    mp3.run()
+
+    #check's if songs has lyrics and if not return a message
+    
--- song.py
+++ song.py
@@ -1,3 +1,4 @@
+import math
 class Song:
     def __init__(self, name, artist, album, length):
         self.name = name
@@ -5,23 +6,8 @@ class Song:
         self.album = album
         self.length = int(length)
         
-        if self.isIn(1,self.name,songs,artist,album)==False:
-            songs[len(songs)+1]=self.name
-        if  self.isIn(2,self.artist,songs,artist,album)==False:
-            songs[len(songs)+1]=self.name
-        if  self.isIn(3,self.album,songs,artist,album)==False:
-            songs[len(songs)+1+1]=self.name
-
 # HW: fix... s.get_name_of_song() == s.name
-#מחזיר את הזמר
-    def get_name_of_song(self):
-        print(self.name)
-#מחזיר את הזמר
-    def get_artist(self):
-        print(self.artist)
-#מחזיר את האלבום בו השיר נמצא
-    def get_album(self):
-        print(self.album)
+
 #מחזיר את אורך השיר בשניות
     def get_length(self):
         print(self.length)
@@ -37,25 +23,3 @@ class Song:
         sec=str(self.length%60)
         r=(f"Songs length is:" +min+ ":" +sec +" minutes ")
         return r
-#בודק האם ערך מסוים נמצא ב1 מהמילונים ואם לו מוסיף את הערך
-    def isIn(self,x,value,songs,artist,album):
-        number=int(x)
-        bool=False
-        if number==1:
-         if 'value' in songs.values()==True:
-            bool=True
-         else:
-          pass
-
-         if number==2:
-          if 'value' in artist.values()==True:
-             bool=True
-         else:
-             pass
-
-         if number==3:
-              if 'value' in album.values()==True:
-                 bool=True
-              else:
-                   pass    
-        return bool
